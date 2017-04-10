1bec5fefbddc28d7072f4c021dcfa64186f94ba7
Graham Watson <grahamj.watson@gmail.com>
2017-04-10 16:41:27 +0100

    Initial Commit

---
+++ Accounts Buddy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Accounts Buddy
+//
+//  Created by Graham Watson on 10/04/2017.
+//  Copyright © 2017 Graham Watson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
